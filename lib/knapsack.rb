d462c13cf619b1eafc575ed17198fe6e9de671ae
Steven Jenkins <steven.jenkins@gmail.com>
2011-08-30 21:03:41 -0400

    initial implementation and tests for a knapsack

---
+++ lib/knapsack.rb
@@ -0,0 +1,69 @@
+# Since the profits and weights are both indexed, as well
+# as connected (ie, p[i] and w[i] refer to the same item),
+# we'll put them into a single array.
+class Knapsack
+  # This will fit in a Fixnum on 64-bit systems.
+  # If you need a larger  value for max weight or you are on a 32-bit system,
+  # then you are going to # need Bignum's, which are significantly
+  # slower, so you're better off using a 64-bit system or a C/C++ version
+  # that has a Ruby interface instead of a pure Ruby solution.
+  #
+  # N.B.  On my system in Ruby 1.8.7, 2**62 is a Bignum but 2**61 is a Fixnum.
+  INF = 2**61
+  attr_accessor :n, :c, :items
+
+  # We don't do error checking on the Knapsack creation
+  # because there may be recursion in various algorithms
+  # and we don't want to pay the overhead on each iteration.
+  # Instead, we will add a pre-check that will do basic
+  # checks like ensure profits and weights are of same length,
+  # that the items are sorted appropriately, etc.
+  # TODO: Add pre-check validation for Knapsacks
+  def initialize(n, p, w, c)
+    @n = n
+    @c = c
+    @items = []
+    p.each_with_index do |profit, index|
+      @items << Item.new(profit, w[index])
+    end
+    self
+  end
+
+  def p(j)
+    item[p][j]
+  end
+
+  def sum_profits
+    @items.map {|i| i.p}.reduce(0) {|sum,value| sum + value}
+  end
+
+  def sum_weights
+    @items.map {|i| i.w}.reduce(0) {|sum,value| sum + value}
+  end
+end
+
+class Item
+  attr_accessor :p, :w
+  def initialize(profit,weight)
+    validate_profit(profit)
+    validate_weight(weight)
+    @p = profit
+    @w = weight
+    self
+  end
+
+  private
+  def validate_profit(p)
+    validate(p)
+  end
+  def validate_weight(w)
+    validate(w)
+  end
+
+  # profit and weight have same validation
+  def validate(i)
+    unless [Fixnum, Bignum].member?(i.class) and i >= 0
+      raise ArgumentError, "Must be a positive integer, not " + i.to_s
+    end
+  end
+end
--- test/knapsack_test.rb
+++ test/knapsack_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class KnapsackTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_knapsack_creation
+    n = 7
+    p = [70, 20, 39]
+    w = [31, 10, 20]
+    c = 50
+    kp = Knapsack.new(n, p, w, c)
+    assert_equal(kp.class, Knapsack)
+    assert_equal(kp.n, n)
+    assert_equal(kp.c, c)
+    assert_equal(kp.items.length, p.length)
+    # Verify that we kept the mapping between p_i and w_i
+    kp.items.each_with_index do |item, index|
+      assert_equal(item.p, p[index])
+      assert_equal(item.w, w[index])
+    end
+    # Ensure we didn't drop any profits or weights
+    assert_equal(kp.sum_profits, p.reduce(0){|sum,item| sum + item })
+    assert_equal(kp.sum_weights, w.reduce(0){|sum,item| sum + item })
+  end
+end
+
+class ItemTest < Test::Unit::TestCase
+  def test_valid_item_creation
+    p = 3
+    w = 4
+    i = Item.new(p,w)
+    assert_equal(i.class, Item)
+    assert_equal(i.p, p)
+    assert_equal(i.w, w)
+  end
+
+  def test_invalid_profit_item_creation
+    p = -3
+    w = 4
+    assert_raise ArgumentError do
+      Item.new(p,w)
+    end
+    p = 3.5
+    assert_raise ArgumentError do
+      Item.new(p,w)
+    end
+  end
+
+  def test_invalid_weight_item_creation
+    p = 3
+    w = -4
+    assert_raise ArgumentError do
+      Item.new(p,w)
+    end
+    w = 4.5
+    assert_raise ArgumentError do
+      Item.new(p,w)
+    end
   end
 end
